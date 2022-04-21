575f108dab00682561528964ee7c41c52711de68
RenaldyEkaPutra <renaldyputra71@gmail.com>
2022-04-22 01:54:47 +0700

    homework 2

---
+++ week 2/.workspaces/testing123/Testing.sol
@@ -0,0 +1,28 @@
+pragma solidity >=0.4.22 <0.6.0;
+contract Ballot {
+    address chairperson;
+    constructor(uint8 _numProposals) public {
+        function giveRightToVote(address toVoter) public {
+   contract MyContract {
+
+uint amount;
+uint value;
+
+constructor (uint initialAmount, uint initialValue) public {
+amount = 0;
+value = 1000;
+}
+
+function getBalance() public view returns(uint) {
+return value;
+}
+
+function getAmount() public view returns(uint) {
+return amount;
+}
+
+function send(uint newDeposit) public {
+value = value - newDeposit;
+amount = amount + newDeposit;
+}
+}
